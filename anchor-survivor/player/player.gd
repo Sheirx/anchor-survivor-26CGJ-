37daebeb49044f1ce863e9db00a2d3094b167ec1
Sherix <1113149798@qq.com>
2026-07-03 19:28:37 +0800

    11

---
+++ anchor-survivor/player/player.gd
@@ -0,0 +1,12 @@
+extends CharacterBody2D
+var can_laser: bool = true
+var can_gernade: bool = true
+@export var speed:int = 500
+signal laser(pos,direction)
+signal gernade(pos,direction)
+
+func _process(_delta: float) -> void:
+	var direction = Input.get_vector("left","right","up","down")
+	velocity = direction * speed
+	move_and_slide()
+	Globals.player_pos = global_position
