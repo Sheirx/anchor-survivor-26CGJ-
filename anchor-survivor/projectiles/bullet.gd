88df2cdd842e45308524a2c86fee8849a0eb7d3a
ken1-hub <2228371952@qq.com>
2026-07-05 13:16:58 +0800

    Update bullet.gd

--- anchor-survivor/projectiles/bullet.gd
+++ anchor-survivor/projectiles/bullet.gd
@@ -1,12 +1,17 @@
 extends Area2D
 
 @export var speed := 700.0
+@export var lifetime := 1.5
+
 var direction := Vector2.RIGHT
 var damage := 10
 
+func _ready():
+	await get_tree().create_timer(lifetime).timeout
+	queue_free()
+
 func _process(delta):
 	global_position += direction * speed * delta
-
 
 func _on_body_entered(body):
 	if body.has_method("take_damage"):
